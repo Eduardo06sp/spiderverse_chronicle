6c912663196448a4f7a39b6115e7ec5d173e8299
Eduardo <eduardopelaez@pm.me>
2025-12-09 00:50:59 +0000

    Prepare content upon initial script execution

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+const decade = document.querySelector('.decade');
+const comicCovers = document.getElementsByClassName('comic-covers');
+const comics = comicCovers[0].getElementsByClassName('comic');
+
+console.log(decade);
+
+function removeExpandClass(el) {
+  el.classList.remove('expanded');
+};
+
+function addContractClass(el) {
+  el.classList.add('contracted');
+};
+
+function onlyDisplayFirst(els) {
+  total = els.length;
+
+  for (let i = 1; i < total; i++) {
+    els[i].classList.add('hidden');
+  };
+};
+
+/*
+function toggleContent(e) {
+};
+*/
+
+removeExpandClass(decade);
+addContractClass(decade);
+onlyDisplayFirst(comics);
+//decade.addEventListener('pointerdown', toggleContent(e));
